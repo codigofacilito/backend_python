e81b1ef9e2e4f1ad2fb5269b96942307ed0817b8
@eduardo_78d <eduardo78d@gmail.com>
2017-03-01 10:38:16 -0600

    methods

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
+from flask import flash
 
 import datetime
 
 app = Flask(__name__)
 
-@app.route('/login')
-def login():
-	autor = 'Eduardo'
-	tags = ['Python', 'web']
-	ahora = datetime.datetime.now()
-	activo = False
+usuarios = {'eduardo': 'password'}
 
-	return render_template('login.html', autor=autor, tags=tags, ahora=ahora, activo=activo)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+	if request.method == 'POST':
+		username = request.form.get('username')
+		password = request.form.get('password')
+		
+		usuario = usuarios.get(username, None)
+		if usuario and usuario == password:
+			flash('Autenticacion valida!', 'error')
+		else:
+			flash('Usuario o password no validos', 'error')
+
+
+	return render_template('login.html')
 
 if __name__ == "__main__":
 	app.run()
