1d24c071c38ff2b7051e1ee57ee0a638ce69881a
@eduardo_78d <eduardo78d@gmail.com>
2017-03-02 15:31:01 -0600

    session

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from flask import render_template
 from flask import request
 from flask import flash
 
+from flask import session
+from flask import redirect
+from flask import url_for
+
+import os
 import datetime
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('flask_key')
 
-usuarios = {'eduardo': 'password'}
+class User:
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+	def valid_password(self, password):
+		return self.password == password
+
+first_user = User('eduardo', '123')
+second_user = User('cf', '123')
+users = { first_user.username : first_user, second_user.username : second_user}
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,14 +32,32 @@ def login():
 		username = request.form.get('username')
 		password = request.form.get('password')
 		
-		usuario = usuarios.get(username, None)
-		if usuario and usuario == password:
-			flash('Autenticacion valida!', 'error')
-		else:
-			flash('Usuario o password no validos', 'error')
+		user = users.get(username, None)
+		if user and user.valid_password(password):
+			message = 'Bienvenido usuario {}'.format(username)
+			flash(message, 'success')
 
+			session['user'] = username
+			return redirect(url_for('dashboard'))
+
+		else:
+			flash('Usuario o password invalidos.', 'danger')
 
 	return render_template('login.html')
 
+
+@app.route('/dashboard')
+def dashboard():
+	username = session['user']
+	return render_template('dashboard.html', username=username)
+
+
+@app.route('/logout')
+def logout():
+	session.pop('user')
+	return redirect( url_for('login'))
+
+
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
